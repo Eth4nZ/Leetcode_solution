7a5b9ca3ebca57256c8d4c68e8e39aaf55203196
ethan <aikszhang@gmail.com>
2017-08-07 22:02:04 +0800

    Add solution 2 for problem 3 and Update gitignore

--- 3. Longest Substring Without Repeating Characters/longest_substring_without_repeating_characters_test.go
+++ 3. Longest Substring Without Repeating Characters/longest_substring_without_repeating_characters_test.go
@@ -7,15 +7,42 @@ func TestLengthOfLongestSubstring(test *testing.T) {
 		"abcabcbb",
 		"bbbbb",
 		"pwwkew",
+		"abacde",
+		"jabjcdel",
 	}
 	results := []int{
 		3,
 		1,
 		3,
+		5,
+		7,
 	}
 
 	for t := 0; t < len(tests); t++ {
 		if ret := lengthOfLongestSubstring(tests[t]); ret != results[t] {
+			test.Fatalf("case %d failed. found: %v, expect: %v \n", t, ret, results[t])
+		}
+	}
+}
+
+func TestLengthOfLongestSubstring2(test *testing.T) {
+	tests := []string{
+		"abcabcbb",
+		"bbbbb",
+		"pwwkew",
+		"abacde",
+		"jabjcdel",
+	}
+	results := []int{
+		3,
+		1,
+		3,
+		5,
+		7,
+	}
+
+	for t := 0; t < len(tests); t++ {
+		if ret := lengthOfLongestSubstring2(tests[t]); ret != results[t] {
 			test.Fatalf("case %d failed. found: %v, expect: %v \n", t, ret, results[t])
 		}
 	}
