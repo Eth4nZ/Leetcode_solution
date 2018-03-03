b8f3071eb0f8e66befd83f7d75661da46bfd94ff
ethan <aikszhang@gmail.com>
2018-03-03 23:03:17 +1100

    Add dp solutions for problem 5

--- leetcode-kotlin/src/main/kotlin/_5_longest_palindromic_substring/LongestPalindromicSubstring.kt
+++ leetcode-kotlin/src/main/kotlin/_5_longest_palindromic_substring/LongestPalindromicSubstring.kt
@@ -70,4 +70,110 @@ class LongestPalindromicSubstring {
         return curLongestPalindrome
     }
 
+    var longestString = ""
+
+    fun longestPalindromeDP0(s: String): String {
+        if (s.length <= 1)
+            return s
+        longestLen(s, 0, s.length-1)
+        return if (longestString == "")
+            s[0].toString()
+        else
+            longestString
+    }
+
+    fun longestLen(s: String, lhs: Int, rhs: Int): Int {
+        if (s[lhs] == s[rhs]){
+            if (rhs - lhs <= 2) {
+                if (s.substring(lhs, rhs+1).length > longestString.length) {
+                    longestString = s.substring(lhs, rhs+1)
+                }
+                return rhs - lhs +1
+            } else
+                return longestLen(s, lhs + 1, rhs - 1) + 2
+        }
+        else {
+            if (rhs - lhs <= 1)
+                return 0
+            else {
+                return maxOf(longestLen(s, lhs + 1, rhs), longestLen(s, lhs, rhs - 1))
+            }
+        }
+
+    }
+
+    fun longestPalindromeDP1(s: String): String {
+        if (s.length <= 1)
+            return s
+        val str = longestLen1(s)
+        return if (str == "")
+            s[0].toString()
+        else
+            str
+
+    }
+
+    private fun longestLen1(s: String): String {
+        if (s.first() == s.last()){
+            if (s.length <= 3) {
+                return s
+            } else {
+                val subString = longestLen1(s.substring(1, s.lastIndex))
+                return if (subString == "")
+                    ""
+                else
+                    s.first() + subString + s.last()
+            }
+        }
+        else {
+            return if (s.length <= 2)
+                ""
+            else {
+                maxLen(longestLen1(s.substring(0, s.lastIndex)), longestLen1(s.substring(1, s.length)))
+            }
+        }
+
+    }
+
+    fun maxLen(s0: String, s1: String): String {
+        return if (s0.length > s1.length)
+            s0
+        else
+            s1
+    }
+
+
+    fun longestPalindromeDP2(s: String): String {
+        var ret = ""
+        val dp = Array(s.length, { BooleanArray(s.length) })
+        for (i in s.lastIndex downTo 0) {
+            for (j in i..s.lastIndex) {
+                dp[i][j] = s[i] == s[j] && (j - i <= 2 || dp[i + 1][j - 1])
+                if (dp[i][j] && j - i + 1 > ret.length)
+                    ret = s.substring(i, j + 1)
+
+            }
+        }
+
+        return ret
+    }
+
+
+    fun longestPalindromeDP3(s: String): String {
+        var ret = ""
+        val dp = Array(s.length, { BooleanArray(s.length) })
+        for (i in 0..s.lastIndex) {
+            for (j in i downTo 0) {
+                dp[i][j] = s[i] == s[j] && (i - j <= 2 || dp[i - 1][j + 1])
+                if (dp[i][j] && i - j + 1 > ret.length)
+                    ret = s.substring(j, i + 1)
+
+            }
+        }
+
+        return ret
+    }
+
+
+
 }
--- leetcode-kotlin/src/test/kotlin/_5_longest_palindromic_substring/TestLongestPalindromicSubstring.kt
+++ leetcode-kotlin/src/test/kotlin/_5_longest_palindromic_substring/TestLongestPalindromicSubstring.kt
@@ -4,40 +4,100 @@ package _5_longest_palindromic_substring
 import org.junit.Assert.assertArrayEquals
 import org.junit.Assert.assertEquals
 import org.junit.Test
+    val tests = listOf(
+            "aaabaaaa",
+            "babadada",
+            "",
+            "cbbd",
+            "abcba",
+            "aa",
+            "aaa",
+            "aaaa",
+            "aaaaa",
+            "babad",
+            "baabaad",
+            "aaabcbaaa",
+            "abbbc",
+            "abcda",
+            "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    )
+    val results = listOf(
+            "aaabaaa",
+            "adada",
+            "",
+            "bb",
+            "abcba",
+            "aa",
+            "aaa",
+            "aaaa",
+            "aaaaa",
+            "aba",
+            "aabaa",
+            "aaabcbaaa",
+            "bbb",
+            "a",
+            "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    )
 
-class TestLongestPalindromicSubstring {
+class TestLongestPalindromicSubstring0 {
 
     @Test
     fun testLongestPalindrome() {
-        val tests = listOf(
-                "",
-                "aa",
-                "aaa",
-                "aaaa",
-                "aaaaa",
-                "babad",
-                "cbbd",
-                "baabaad",
-                "aaabcbaaa",
-                "abbbc",
-                "abcda"
-        )
-        val results = listOf(
-                "",
-                "aa",
-                "aaa",
-                "aaaa",
-                "aaaaa",
-                "aba",
-                "bb",
-                "aabaa",
-                "aaabcbaaa",
-                "bbb",
-                "a"
-        )
-
+        val tests = tests
         val data = tests.map { LongestPalindromicSubstring().longestPalindrome0(it) }
         assertArrayEquals(results.toTypedArray(), data.toTypedArray())
+    }
+}
+
+class TestLongestPalindromicSubstringDP0 {
+
+    @Test
+    fun testLongestPalindrome() {
+        val tests = tests
+        val data = tests.map { LongestPalindromicSubstring().longestPalindromeDP0(it) }
+        assertArrayEquals(results.toTypedArray(), data.toTypedArray())
+    }
+}
+
+class TestLongestPalindromicSubstringDP1 {
+
+    @Test
+    fun testLongestPalindrome() {
+        val tests = tests
+        val data = tests.map { LongestPalindromicSubstring().longestPalindromeDP1(it) }
+        assertArrayEquals(results.toTypedArray(), data.toTypedArray())
+    }
+}
+
+
+class TestLongestPalindromicSubstringDP2 {
+
+    @Test
+    fun testLongestPalindrome() {
+        val tests = tests
+        val data = tests.map { LongestPalindromicSubstring().longestPalindromeDP2(it) }
+        assertArrayEquals(results.toTypedArray(), data.toTypedArray())
+    }
+}
+
+
+class TestLongestPalindromicSubstringDP3 {
+
+    @Test
+    fun testLongestPalindrome() {
+        val tests = tests
+        val data = tests.map { LongestPalindromicSubstring().longestPalindromeDP3(it) }
+        assertArrayEquals(results.toTypedArray(), data.toTypedArray())
+    }
+}
+
+
+class TestSubstring {
+
+    @Test
+    fun testSubstring() {
+        assertEquals("abcde", LongestPalindromicSubstring().maxLen("abcde", "bcd"))
 
     }
 }
+
