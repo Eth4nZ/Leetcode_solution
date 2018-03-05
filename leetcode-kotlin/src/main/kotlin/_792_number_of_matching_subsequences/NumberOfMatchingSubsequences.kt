a2c92031567faa724f8a72035ebc41034eeca9b8
ethan <aikszhang@gmail.com>
2018-03-06 00:08:54 +1100

    AC problem 792 with better performance

--- leetcode-kotlin/src/main/kotlin/_792_number_of_matching_subsequences/NumberOfMatchingSubsequences.kt
+++ leetcode-kotlin/src/main/kotlin/_792_number_of_matching_subsequences/NumberOfMatchingSubsequences.kt
@@ -1,5 +1,10 @@
 package _792_number_of_matching_subsequences
 
+import com.sun.org.apache.xpath.internal.operations.Bool
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.math.absoluteValue
+
 
 class NumberOfMatchingSubsequences {
 
@@ -23,4 +28,41 @@ class NumberOfMatchingSubsequences {
         return array.indices.count { array[it] == 0 }
 
     }
+
+//    fun isMatch(dictionary: Array<ArrayList<Int>>, word: String): Boolean{
+//
+//    }
+
+
+    fun numMatchingSubseqDict(S: String, words: Array<String>): Int {
+
+        val dictionary = Array(26, { ArrayList<Int>(0)  })
+        for ((i, c) in S.withIndex()) {
+            dictionary[c-'a'].add(i)
+        }
+        val f = dictionary['g'-'a'].lastIndex
+        var count = 0
+        for (word in words) {
+            var i = 0
+            for (c in word) {
+                var position = dictionary[c - 'a'].binarySearch(i)
+                if (position >= 0) {
+                    i = dictionary[c - 'a'][position] + 1
+                } else
+                    position = -position - 1
+                    if (position > dictionary[c - 'a'].lastIndex) {
+                        i = -1
+                        break
+                    }
+                    else
+                        i = dictionary[c - 'a'][position] + 1
+            }
+            if (i > word.lastIndex){
+                count++
+            }
+        }
+
+        return count
+    }
+
 }
--- leetcode-kotlin/src/test/kotlin/_792_number_of_matching_subsequences/TestNumberOfMatchingSubsequences.kt
+++ leetcode-kotlin/src/test/kotlin/_792_number_of_matching_subsequences/TestNumberOfMatchingSubsequences.kt
@@ -3,21 +3,52 @@ package _792_number_of_matching_subsequences
 import _5_longest_palindromic_substring.tests
 import com.sun.xml.internal.fastinfoset.util.StringArray
 import org.junit.Assert.assertArrayEquals
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 
 class TestNumberOfMatchingSubsequences {
     val tests = listOf(
+            listOf("abcde", arrayOf("bb")),
+            listOf("dsahjpjauf", arrayOf("ja", "ahjpjau","ahbwzgqnuk","tnmlanowax")),
+            listOf("abc", arrayOf("d")),
             listOf("abcde", arrayOf("a", "bb", "acd", "ace"))
             )
     val results = listOf(
+            0,
+            2,
+            0,
             3
             )
 
-        @Test
-        fun testLongestPalindrome() {
-            val tests = tests
-            val data = tests.map { NumberOfMatchingSubsequences().numMatchingSubseq(it[0].toString(), it[1] as Array<String>) }
-            assertArrayEquals(results.toTypedArray(), data.toTypedArray())
-        }
+    @Test
+    fun testLongestPalindrome() {
+        val tests = tests
+        val data = tests.map { NumberOfMatchingSubsequences().numMatchingSubseq(it[0].toString(), it[1] as Array<String>) }
+        assertArrayEquals(results.toTypedArray(), data.toTypedArray())
     }
+
+    @Test
+    fun testLongestPalindromeDict() {
+        val tests = tests
+        val data = tests.map { NumberOfMatchingSubsequences().numMatchingSubseqDict(it[0].toString(), it[1] as Array<String>) }
+        assertArrayEquals(results.toTypedArray(), data.toTypedArray())
+    }
+
+    @Test
+    fun binarySearchTest(){
+        val array = listOf(1, 3, 4 ,6 ,8 , 10)
+        for (i in 0..10)
+            print(array.binarySearch(i).toString() + " ")
+        println()
+        val array0 = listOf(1, 3, 4 ,6 ,8 , 10)
+            println(array0.binarySearch(0))
+        val array1 = listOf(1, 3, 4 ,6 ,8 , 10)
+        println(array1.binarySearch(1))
+        val array2 = listOf(1, 3, 4 ,6 ,8 , 10)
+        println(array2.binarySearch(2))
+        val array3 = listOf(1, 3, 4 ,6 ,8 , 10)
+        println(array3.binarySearch(5))
+    }
+
+}
